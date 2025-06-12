e364d1d29cf2247f9f3effb667762a678974ccb1
InfernoblzDEV <saralthapaliya1@gmail.com>
2025-06-12 20:41:55 +0545

    Done acp

--- MiniCalculator/app/src/main/java/com/saral/minicalculator/MainActivity.java
+++ MiniCalculator/app/src/main/java/com/saral/minicalculator/MainActivity.java
@@ -1,24 +1,58 @@
 package com.saral.minicalculator;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText number1, number2;
+    Button addButton, subtractButton, multiplyButton;
+    TextView resultText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        number1 = findViewById(R.id.number1);
+        number2 = findViewById(R.id.number2);
+        addButton = findViewById(R.id.addButton);
+        subtractButton = findViewById(R.id.subtractButton);
+        multiplyButton = findViewById(R.id.multiplyButton);
+        resultText = findViewById(R.id.resultText);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(number1.getText().toString());
+                double b = Double.parseDouble(number2.getText().toString());
+                double result = a + b;
+                resultText.setText("Result: " + result);
+            }
+        });
+
+        subtractButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(number1.getText().toString());
+                double b = Double.parseDouble(number2.getText().toString());
+                double result = a - b;
+                resultText.setText("Result: " + result);
+            }
+        });
+
+        multiplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double a = Double.parseDouble(number1.getText().toString());
+                double b = Double.parseDouble(number2.getText().toString());
+                double result = a * b;
+                resultText.setText("Result: " + result);
+            }
         });
     }
 }
