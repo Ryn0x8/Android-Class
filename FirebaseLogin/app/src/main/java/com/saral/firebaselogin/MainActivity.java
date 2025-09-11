6d10d5ad29659d56c018b80df34cd4522a2f89f6
InfernoblzDEV <saralthapaliya1@gmail.com>
2025-09-11 20:30:26 +0545

    COmpleted Activity

--- FirebaseLogin/app/src/main/java/com/saral/firebaselogin/MainActivity.java
+++ FirebaseLogin/app/src/main/java/com/saral/firebaselogin/MainActivity.java
@@ -1,19 +1,77 @@
 package com.saral.firebaselogin;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.*;
 
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    EditText name, email;
+    Button add;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        EdgeToEdge.enable(this);
+
+        name = findViewById(R.id.editTextText2);
+        email = findViewById(R.id.editTextTextEmailAddress2);
+        add = findViewById(R.id.button2);
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Student");
+
+
+
+        add.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String fullname = name.getText().toString();
+                        String emailadd = email.getText().toString();
+
+                        if (TextUtils.isEmpty(fullname)){
+                            Toast.makeText(MainActivity.this, "Enter your name", Toast.LENGTH_SHORT).show();
+                        } else if (TextUtils.isEmpty(emailadd)) {
+                            Toast.makeText(MainActivity.this, "Enter your email", Toast.LENGTH_SHORT).show();
+
+                        }
+                        else {
+                            Student student = new Student(fullname, emailadd);
+                            String uniqueid = reference.push().getKey();
+                            reference.child(Objects.requireNonNull(uniqueid)).setValue(student).addOnCompleteListener(
+                                    new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()){
+                                                Toast.makeText(MainActivity.this, "Data Added Successfully", Toast.LENGTH_SHORT).show();
+                                                name.setText("");
+                                                email.setText("");
+                                            }
+                                            else{
+                                                Toast.makeText(MainActivity.this, "Failed to insert data", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    }
+                            );
+                        }
+                    }
+                }
+        );
     }
 }
---
+++ FirebaseLogin/app/src/main/java/com/saral/firebaselogin/Student.java
@@ -0,0 +1,30 @@
+package com.saral.firebaselogin;
+
+public class Student {
+    String name, email;
+
+    public Student() {
+    }
+
+    public Student(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
