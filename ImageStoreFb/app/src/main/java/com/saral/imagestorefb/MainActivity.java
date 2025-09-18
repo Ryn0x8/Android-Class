dbae56f2fb011fecbc850b898786c6d3af605b25
InfernoblzDEV <saralthapaliya1@gmail.com>
2025-09-18 17:45:39 +0545

    ACP DOne

--- ImageStoreFb/app/src/main/java/com/saral/imagestorefb/MainActivity.java
+++ ImageStoreFb/app/src/main/java/com/saral/imagestorefb/MainActivity.java
@@ -1,28 +1,26 @@
 package com.saral.imagestorefb;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.Firebase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
 
 public class MainActivity extends AppCompatActivity {
-     ImageView img;
+    ImageView img;
     Button select, upload;
     StorageReference reference;
     Uri imageURI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,48 +33,56 @@ public class MainActivity extends AppCompatActivity {
 
         reference = FirebaseStorage.getInstance().getReference().child("images");
 
-        select.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent();
-                        intent.setType("images/*");
-                        intent.setAction(Intent.ACTION_GET_CONTENT);
-                        startActivityForResult(intent, 123);
-                    }
-                }
-        );
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("images/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 123);
+            }
+        });
 
-        upload.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        StorageReference storageReference = reference.child(String.valueOf(System.currentTimeMillis()));
-                        
-                        storageReference.putFile(imageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                                if (task.isSuccessful()){
-                                    Toast.makeText(MainActivity.this, "Successfully Uploaded", Toast.LENGTH_SHORT).show();
-                                    img.setImageResource(R.drawable.ic_launcher_foreground);
-                                }
-                            }
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (imageURI == null) {
+                    Toast.makeText(MainActivity.this, "Please select an image first", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("Uploading...");
+                progressDialog.setMessage("Please wait while your image is being uploaded");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+
+                StorageReference storageReference = reference.child(String.valueOf(System.currentTimeMillis()));
+
+                storageReference.putFile(imageURI)
+                        .addOnSuccessListener(taskSnapshot -> {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Successfully Uploaded", Toast.LENGTH_SHORT).show();
+                            img.setImageResource(R.drawable.ic_launcher_foreground);
+                        })
+                        .addOnFailureListener(e -> {
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Upload Failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        })
+                        .addOnProgressListener(taskSnapshot -> {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            progressDialog.setMessage("Uploaded " + (int) progress + "%");
                         });
-                    }
-                }
-        );
-
-
-
+            }
+        });
     }
 
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == RESULT_OK && requestCode == 123 && data !=null){
+        if (resultCode == RESULT_OK && requestCode == 123 && data != null) {
             imageURI = data.getData();
             img.setImageURI(imageURI);
         }
     }
-
 }
