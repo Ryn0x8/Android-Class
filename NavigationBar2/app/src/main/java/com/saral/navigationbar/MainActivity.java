56407c6b5aade2df102bda164114bd12520b7ee3
InfernoblzDEV <saralthapaliya1@gmail.com>
2025-09-25 20:34:34 +0545

    Done Project

---
+++ NavigationBar2/app/src/main/java/com/saral/navigationbar/MainActivity.java
@@ -0,0 +1,35 @@
+package com.saral.navigationbar;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+public class MainActivity extends AppCompatActivity {
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    public Toolbar toolbar;
+    @SuppressLint("UseSupportActionBar")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.main);
+        toolbar = findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.nav_open, R.string.nav_close);
+
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+
+    }
+}
