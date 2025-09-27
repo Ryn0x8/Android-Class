6dc87f9ff044556b892c819e34e3e6a8254f2472
InfernoblzDEV <saralthapaliya1@gmail.com>
2025-09-27 14:34:03 +0545

    Update

--- WebView/app/src/main/java/com/saral/webview/MainActivity.java
+++ WebView/app/src/main/java/com/saral/webview/MainActivity.java
@@ -1,15 +1,20 @@
 package com.saral.webview;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     WebView webView;
+    ProgressBar progressBar;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,39 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("https://www.codingal.com");
+        progressBar = findViewById(R.id.progressBar2);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                progressBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.INVISIBLE);
+                super.onPageFinished(view, url);
+            }
+        });
+        webView.loadUrl("https://www.google.com");
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (webView.canGoBack()){
+                    webView.goBack();
+                }else{
+                    setEnabled(false);
+                }
+            }
+        };
+
+        getOnBackPressedDispatcher().addCallback(callback);
+
+
+
+
     }
+
+
+
 }
