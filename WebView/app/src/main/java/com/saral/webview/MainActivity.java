22ba422244a83739c55aa1a1f555f8c03113e8ae
InfernoblzDEV <saralthapaliya1@gmail.com>
2025-09-27 13:13:23 +0545

    Completed  Activity

---
+++ WebView/app/src/main/java/com/saral/webview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.saral.webview;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.codingal.com");
+    }
+}
