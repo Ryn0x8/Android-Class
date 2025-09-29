e4f512bcb91e3ac11fefc364cb58e47b7089a406
InfernoblzDEV <saralthapaliya1@gmail.com>
2025-09-29 11:44:08 +0545

    DONE ACP

---
+++ GoogleMapACP/app/src/main/java/com/saral/googlemapacp/MainActivity.java
@@ -0,0 +1,40 @@
+
+package com.saral.googlemapacp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button launchMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        launchMap = findViewById(R.id.button);
+
+        launchMap.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse("geo:27.7172,85.3240"));
+                        Intent chooser = Intent.createChooser(i, "Launch Maps");
+                        startActivity(chooser);
+                    }
+                }
+        );
+
+    }
+}
